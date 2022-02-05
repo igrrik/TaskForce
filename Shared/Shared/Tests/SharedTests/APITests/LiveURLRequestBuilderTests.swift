6796ef1f4ee317e70aa5ad628719cbf8c73aae0d
igrrik <igrrik@mail.ru>
2022-02-06 01:03:51 +0300

    Update LiveURLRequestBuilderTests

--- Shared/Shared/Tests/SharedTests/APITests/LiveURLRequestBuilderTests.swift
+++ Shared/Shared/Tests/SharedTests/APITests/LiveURLRequestBuilderTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class LiveURLRequestBuilderTests: XCTestCase {
     private var sut: LiveURLRequestBuilder!
-    private var hasher: ((String) -> String)!
+    private var hasher: StubHasher!
     private var randomStringProvider: (() -> String)!
     private let publicKey = "123"
     private let privateKey = "abc"
@@ -19,14 +19,14 @@ final class LiveURLRequestBuilderTests: XCTestCase {
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-        hasher = { $0.uppercased() }
+        hasher = .init()
         randomStringProvider = { "z42" }
         sut = LiveURLRequestBuilder(
             scheme: scheme,
             host: host,
             privateKey: privateKey,
             publicKey: publicKey,
-            hasher: { $0.md5()! },
+            hasher: hasher,
             randomStringProvider: randomStringProvider
         )
     }
@@ -36,22 +36,49 @@ final class LiveURLRequestBuilderTests: XCTestCase {
         try super.tearDownWithError()
     }
 
-    func testThatURLRequestIsNil() {}
-
     func testThatURLRequestIsCreated() {
-        // arrange        
+        // arrange
+        hasher.hashFunction = { $0.uppercased() }
         let expectedString = "https://example.com/v1/public/characters?apikey=123&ts=z42&hash=Z42ABC123&limit=10&offset=350"
 
         // act
-        let urlRequest = sut.makeURLRequest(from: ObtainCharactersRequest(limit: 10, offset: 350))
+        let urlRequest = try! sut.makeURLRequest(from: ObtainCharactersRequest(limit: 10, offset: 350))
 
         // assert
-        guard let urlRequest = urlRequest else {
-            XCTFail("URLRequest shouldn't be nil")
-            return
-        }
         XCTAssertEqual(urlRequest.url!.absoluteString, expectedString)
         XCTAssertEqual(urlRequest.httpMethod, HTTPMethod.GET.rawValue)
         XCTAssertEqual(urlRequest.allHTTPHeaderFields, ["Accept": "application/json"])
     }
+
+    func testThatURLConstructionFailureIsThrownWhenURLCannotBeObtainedFromComponents() {
+        // arrange
+        hasher.hashFunction = { $0.uppercased() }
+
+        // act
+        do {
+            let _ = try sut.makeURLRequest(from: FailingRequest())
+            XCTFail("Make URL request should throw error")
+        } catch let error as LiveURLRequestBuilder.URLConstructionFailure {
+            // assert
+            XCTAssertNotNil(error.errorDescription)
+        } catch {
+            XCTFail("Unexpected error")
+        }
+    }
+}
+
+private final class StubHasher: Hasher {
+    var hashFunction: ((String) -> String)!
+
+    func hash(_ string: String) throws -> String {
+        hashFunction(string)
+    }
+}
+
+private struct FailingRequest: APIRequest {
+    typealias ResultElement = Character
+    typealias Response = APISingleElementResponse<ResultElement>
+
+    let endpoint: APIEndpoint = .init(base: "v1/public", resource: .characters)
+    let queryItems: [URLQueryItem] = []
 }
