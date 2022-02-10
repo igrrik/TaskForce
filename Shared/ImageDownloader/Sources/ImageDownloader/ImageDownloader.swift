9587958ea29814941eca017d9edc188bbdb5eb0c
igrrik <igrrik@mail.ru>
2022-02-11 02:28:08 +0300

    Add ImageDownloader Package

---
+++ Shared/ImageDownloader/Sources/ImageDownloader/ImageDownloader.swift
@@ -0,0 +1,34 @@
+//
+//  ImageDownloader.swift
+//
+//
+//  Created by Igor Kokoev on 07.02.2022.
+//
+
+import UIKit
+import Combine
+import Kingfisher
+
+public protocol ImageDownloader {
+    func downloadImage(with url: URL) -> AnyPublisher<UIImage, Error>
+    func prefetchImages(with urls: [URL])
+}
+
+public final class KingfisherImageDownloader: ImageDownloader {
+    private let kingfisher: KingfisherManager
+
+    public init(kingfisher: KingfisherManager = .shared) {
+        self.kingfisher = kingfisher
+    }
+
+    public func downloadImage(with url: URL) -> AnyPublisher<UIImage, Error> {
+        // TODO: Check for deferred
+        ImagePublisher(configuration: .url(url, kingfisher))
+            .eraseToAnyPublisher()
+    }
+
+    public func prefetchImages(with urls: [URL]) {
+        ImagePrefetcher(urls: urls, options: [.downloader(kingfisher.downloader)])
+            .start()
+    }
+}
---
+++ Shared/ImageDownloader/Sources/ImageDownloader/ImagePublisher.swift
@@ -0,0 +1,60 @@
+//
+//  ImagePublisher.swift
+//  
+//
+//  Created by Igor Kokoev on 11.02.2022.
+//
+
+import UIKit
+import Combine
+import Kingfisher
+
+struct ImagePublisher: Publisher {
+    typealias Output = UIImage
+    typealias Failure = Error
+
+    enum Configuration {
+        case failure(Error)
+        case url(URL, KingfisherManager)
+    }
+
+    let configuration: Configuration
+
+    func receive<S>(subscriber: S) where S: Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+        let subscription = ImageProviderSubscription(subscriber: subscriber, configuration: configuration)
+        subscriber.receive(subscription: subscription)
+    }
+}
+
+private final class ImageProviderSubscription<S: Subscriber>: Subscription
+where S.Input == UIImage, S.Failure == Error {
+    private let configuration: ImagePublisher.Configuration
+    private var subscriber: S?
+    private var downloadTask: DownloadTask?
+
+    init(subscriber: S, configuration: ImagePublisher.Configuration) {
+        self.subscriber = subscriber
+        self.configuration = configuration
+    }
+
+    func request(_ demand: Subscribers.Demand) {
+        switch configuration {
+        case let .failure(error):
+            _ = subscriber?.receive(completion: .failure(error))
+        case let .url(url, kingfisher):
+            downloadTask = kingfisher.retrieveImage(with: url) { [weak self] result in
+                switch result {
+                case .success(let imageResult):
+                    _ = self?.subscriber?.receive(imageResult.image)
+                case .failure(let error):
+                    _ = self?.subscriber?.receive(completion: .failure(error))
+                }
+            }
+        }
+    }
+
+    func cancel() {
+        downloadTask?.cancel()
+        subscriber = nil
+    }
+}
