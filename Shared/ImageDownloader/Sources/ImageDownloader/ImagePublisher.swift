4c9bfa9210d299bd37a4c24330ac1fe83256840d
igrrik <igrrik@mail.ru>
2022-02-13 10:50:46 +0300

    Update ImagePublisher

--- Shared/ImageDownloader/Sources/ImageDownloader/ImagePublisher.swift
+++ Shared/ImageDownloader/Sources/ImageDownloader/ImagePublisher.swift
@@ -9,47 +9,67 @@ import UIKit
 import Combine
 import Kingfisher
 
+protocol ImageDownloadTask {
+    func cancel()
+}
+
+protocol ImageRetriver {
+    func retrieveImage(with url: URL, completionHandler: ((Result<UIImage, Error>) -> Void)?) -> ImageDownloadTask?
+}
+
+extension Kingfisher.DownloadTask: ImageDownloadTask {}
+
+extension KingfisherManager: ImageRetriver {
+    func retrieveImage(with url: URL, completionHandler: ((Result<UIImage, Error>) -> Void)?) -> ImageDownloadTask? {
+        retrieveImage(with: url, options: nil, progressBlock: nil, downloadTaskUpdated: nil) { result in
+            switch result {
+            case .success(let imageResult):
+                completionHandler?(.success(imageResult.image))
+            case .failure(let error):
+                completionHandler?(.failure(error))
+            }
+        }
+    }
+}
+
 struct ImagePublisher: Publisher {
     typealias Output = UIImage
     typealias Failure = Error
 
-    enum Configuration {
-        case failure(Error)
-        case url(URL, KingfisherManager)
-    }
-
-    let configuration: Configuration
+    let url: URL
+    let imageRetriever: ImageRetriver
 
     func receive<S>(subscriber: S) where S: Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
-        let subscription = ImageProviderSubscription(subscriber: subscriber, configuration: configuration)
+        let subscription = ImageProviderSubscription(
+            subscriber: subscriber,
+            url: url,
+            imageRetriever: imageRetriever
+        )
         subscriber.receive(subscription: subscription)
     }
 }
 
 private final class ImageProviderSubscription<S: Subscriber>: Subscription
 where S.Input == UIImage, S.Failure == Error {
-    private let configuration: ImagePublisher.Configuration
+    private let url: URL
+    private let imageRetriever: ImageRetriver
     private var subscriber: S?
-    private var downloadTask: DownloadTask?
+    private var downloadTask: ImageDownloadTask?
 
-    init(subscriber: S, configuration: ImagePublisher.Configuration) {
+    init(subscriber: S, url: URL, imageRetriever: ImageRetriver) {
         self.subscriber = subscriber
-        self.configuration = configuration
+        self.url = url
+        self.imageRetriever = imageRetriever
     }
 
-    func request(_ demand: Subscribers.Demand) {
-        switch configuration {
-        case let .failure(error):
-            _ = subscriber?.receive(completion: .failure(error))
-        case let .url(url, kingfisher):
-            downloadTask = kingfisher.retrieveImage(with: url) { [weak self] result in
-                switch result {
-                case .success(let imageResult):
-                    _ = self?.subscriber?.receive(imageResult.image)
-                    self?.subscriber?.receive(completion: .finished)
-                case .failure(let error):
-                    _ = self?.subscriber?.receive(completion: .failure(error))
-                }
+    func request(_ demand: Subscribers.Demand) {        
+        downloadTask = imageRetriever.retrieveImage(with: url) { [weak self] result in
+            switch result {
+            case .success(let image):
+                _ = self?.subscriber?.receive(image)
+                self?.subscriber?.receive(completion: .finished)
+            case .failure(let error):
+                _ = self?.subscriber?.receive(completion: .failure(error))
             }
         }
     }
