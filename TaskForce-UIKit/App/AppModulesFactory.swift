4c3c35599022b895672d79b4030a80d0fd2137a0
igrrik <igrrik@mail.ru>
2022-02-13 11:22:36 +0300

    Add AppModulesFactory

---
+++ TaskForce-UIKit/App/AppModulesFactory.swift
@@ -0,0 +1,42 @@
+//
+//  AppModulesFactory.swift
+//  TaskForce-UIKit
+//
+//  Created by Igor Kokoev on 13.02.2022.
+//
+
+import UIKit
+import TaskForceCore
+
+public final class AppModulesFactory {
+    private lazy var requestBuilder = LiveURLRequestBuilder(
+        scheme: "https",
+        host: "gateway.marvel.com",
+        privateKey: CredentialsStore.shared.privateKey,
+        publicKey: CredentialsStore.shared.publicKey,
+        hasher: MD5Hasher(),
+        randomStringProvider: Date.provideTimestampString
+    )
+    private lazy var apiClient = LiveAPIClient(
+        urlRequestBuilder: requestBuilder,
+        session: .shared,
+        decoder: JSONDecoder(),
+        delegateQueue: .main
+    )
+    private lazy var charactersRepository = LiveCharactersRepository(apiClient: apiClient)
+
+    func makeCharactersListModule() -> UIViewController {
+        let viewModel = CharactersListViewModel(charactersRepository: charactersRepository)
+        return CharactersListViewController(viewModel: viewModel)
+    }
+
+    func makeCharacterDetailsModule(_ character: Character) -> UIViewController {
+        fatalError()
+    }
+}
+
+private extension Date {
+    static func provideTimestampString() -> String {
+        "\(Date().timeIntervalSince1970)"
+    }
+}
